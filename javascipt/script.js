5dd2a769973ea45346576b5edc2317e42658356a
preshu1 <shresthapreshu26@gmail.com>
2025-11-02 11:07:51 +0545

    changed logic so that only one question and answer can be viewed

--- javascipt/script.js
+++ javascipt/script.js
@@ -1,8 +1,25 @@
 const faqContainer = document.querySelector(".faq-container");
 
-faqContainer.addEventListener("click", function (event) {
+/*faqContainer.addEventListener("click", function (event) {
   if (event.target.classList.contains("faq-question")) {
     const faq = event.target.parentElement;
     faq.classList.toggle("active");
+  }
+});*/
+
+//only open one faq at a time
+
+faqContainer.addEventListener("click", function (event) {
+  if (event.target.classList.contains("faq-question")) {
+    const clickedFaq = event.target.parentElement;
+
+    const allFaq = faqContainer.querySelectorAll(".faq");
+    allFaq.forEach((faq) => {
+      if (faq !== clickedFaq) {
+        faq.classList.remove("active");
+      }
+    });
+
+    clickedFaq.classList.toggle("active");
   }
 });
